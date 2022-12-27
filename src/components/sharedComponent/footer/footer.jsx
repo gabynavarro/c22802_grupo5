bc4879d5f8de09458379dfafb967eb46f35c8371
Gabriel Navarro <alt64b@gmail.com>
2022-12-27 20:22:58 -0300

    footer

---
+++ src/components/sharedComponent/footer/footer.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import '../footer/footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook,faTelegram,faGithub,faTwitter} from '@fortawesome/free-brands-svg-icons'
+
+
+const Footer=()=> {
+    
+        return (
+            <div className='footer-container'>
+                <footer className="footer-mynetflix">
+                    <div className="container">                       
+                            <div className="social-group-icon">                               
+                                <FontAwesomeIcon icon={faTwitter} className="social-icon"/>
+                                <FontAwesomeIcon icon={faGithub} className="social-icon"/>                         
+                                <FontAwesomeIcon icon={faFacebook} className="social-icon"/>                         
+                            </div>
+                            <p>Copyright © 2017 - GnSoluciones</p>
+                       
+                    </div>
+                </footer>
+            </div>
+        );
+    }
+
+
+export default Footer;
--- src/pages/HomeScreen.jsx
+++ src/pages/HomeScreen.jsx
@@ -1,13 +1,14 @@
 import Nav from '../components/sharedComponent/nav/Nav'
 import Carousel from '../components/carousel/Carousel'
 import MainMovie from '../components/main-movie/Main-Movie'
+import Footer from '../components/sharedComponent/footer/footer'
 export const HomeScreem=()=>{
     return(
         <div>
         <Nav></Nav>
         <MainMovie/>
         <Carousel/>
-        
+        <Footer/>
         </div>
     )        
 }
