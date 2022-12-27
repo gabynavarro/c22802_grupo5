8635ebe8dc639cacc6c90496f7b7fd9654fcfb5c
Gabriel Navarro <alt64b@gmail.com>
2022-12-26 22:34:45 -0300

    slider full

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,43 +1,93 @@
-import React from 'react'
-import '../carousel/carousel.css'
-const Carousel = () => {
+import React, { useRef, useState, useEffect } from "react";
+import {
+  BBrowserRouter as Router, Routes, Route, 
+  Link, 
+  useNavigate, useParams, useLocation,
+  }from 'react-router-dom'
+import CardMovie from "../card-movie/CardMovie";
+import { Swiper, SwiperSlide } from "swiper/react";
+import 'swiper/css';
+import "swiper/css/pagination"
+import "swiper/css/navigation"
+import SwiperCore, {
+  Pagination,Navigation
+} from 'swiper/core';
+
+SwiperCore.use([Pagination,Navigation]);
+
+
+const Carousel = ({categoria, subcategoria}) => {
+  categoria='movie'
+  subcategoria='popular'
+  console.log(categoria)
+
+  console.log(subcategoria)
+  const history = useNavigate();
+ 
+  const BASE_URL = `https://api.themoviedb.org/3`
+  const APIKEY = `c30046e601e1f588297bc67b7f52c812`;
+  let queryParams = `?language=en-US&page=1&api_key=${APIKEY}`
+  let ruta = ``
+  console.log(ruta)
+
+
+  const concatenarRuta = (pathASumar) => {
+    ruta = ruta + pathASumar
+  }
+   
+
+  {categoria === `movie` && concatenarRuta("/movie")}
+  {categoria === `tv` &&  concatenarRuta("/tv")}
+  {categoria === `trendingMovie` && concatenarRuta(`/trending/movie/week`)}
+  {categoria === `trendingSerie` && concatenarRuta(`/trending/tv/week`)}
+  {subcategoria === `topRated` && concatenarRuta(`/top_rated`)}
+  {subcategoria === `popular` && concatenarRuta(`/popular`)}
+  {subcategoria === `nowPlaying` && concatenarRuta(`/now_playing`)}
+  {subcategoria === `upcoming` && concatenarRuta(`/upcoming`)}
+  {subcategoria === `onTheAir` && concatenarRuta(`/on_the_air`)}
+
+  console.log(ruta)
+
+  const [resultados, setResultados] = useState([]);
+
+  useEffect(() => {
+      
+      const searchString = `${BASE_URL}${ruta}${queryParams}`
+      console.log(searchString)
+
+      fetch(searchString)
+          .then(res => res.json())
+          .then(data => {
+              console.log(data.results)
+              setResultados(data.results)
+          })
+  }, []);
+
   return (
-    <div class="popular-movies carousel-container">
-    <div class="container-title-controls">
-      <h3>Películas Recomendadas</h3>
-      <div class="controls-index"></div>
-    </div>
-
-    <div class="container-principal">
-      <button role="button" id="left-arrow" class="left-arrow"><i class="fas fa-angle-left"></i></button>
-
-      <div class="container-carousel">
-        <div class="carousel">
-          <div class="movie-carousel">
-            <a href="#"><img src="../../assets/img/1.png" alt=""/></a>
-          </div>
-          <div class="movie-carousel">
-            <a href="#"><img src="../../assets/img/2.png" alt=""/></a>
-          </div>
-          <div class="movie-carousel">
-            <a href="#"><img src="../../assets/img/3.png" alt=""/></a>
-          </div>
-          <div class="movie-carousel">
-            <a href="#"><img src="../../assets/img/4.png" alt=""/></a>
-          </div>
-          <div class="movie-carousel">
-            <a href="#"><img src="../../assets/img/5.png" alt=""/></a>
-          </div>
-          <div class="movie-carousel">
-            <a href="#"><img src="../../assets/img/6.png" alt=""/></a>
-          </div>
-         
-        </div>
-      </div>
-
-      <button role="button" id="right-arrow" class="right-arrow"><i class="fas fa-angle-right"></i></button>
-    </div>
-  </div>
+    <>
+    <Swiper slidesPerView={4} 
+              spaceBetween={4} 
+              slidesPerGroup={3} 
+              loop={true} 
+              loopFillGroupWithBlank={true} 
+              navigation={true} 
+              className="mySwiper">
+          {
+            resultados.map((resultado) => {
+                return (
+                  <div key={resultado.id}>      
+                      <SwiperSlide  pagination={true} modules={[Pagination]}>
+                          <CardMovie
+                              resultado={resultado}
+                              categoria={categoria}
+                              />
+                      </SwiperSlide>
+                  </div>
+                )
+            })
+        }       
+      </Swiper>
+    </>
   )
 }
 
